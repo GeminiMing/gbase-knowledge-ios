80ff60a85dc7ff62f56084e67463422a9f59cd41
tengyuming <tym0526@qq.com>
2025-10-31 08:13:26 +0800

    Initial Commit

---
+++ GBaseKnowledgeApp/GBaseKnowledgeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  GBaseKnowledgeAppApp.swift
+//  GBaseKnowledgeApp
+//
+//  Created by apple on 2025/10/31.
+//
+
+import SwiftUI
+
+@main
+struct GBaseKnowledgeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
